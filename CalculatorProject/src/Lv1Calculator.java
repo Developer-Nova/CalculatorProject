1fc8a3b3d4cabc41d3ef112b0d4cbd2ed6fa2713
Nova <woohyeon3382@naver.com>
2024-11-12 12:19:33 +0900

    feat: create getStart() method

--- CalculatorProject/src/Lv1Calculator.java
+++ CalculatorProject/src/Lv1Calculator.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Lv1Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        getStart(scanner);
+    }
+
+    private static void getStart(Scanner scanner) {
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstValue = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondValue = scanner.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operator = scanner.nextLine();
+
+    }
 }
